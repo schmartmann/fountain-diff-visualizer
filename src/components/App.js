989836a7b010bde568b15d5fe764fc7de805a757
Stefan Hartmann <stefanhartmann@gmail.com>
2018-03-17 16:29:12 -0400

    Organize repo into directories

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../stylesheets/Style.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
