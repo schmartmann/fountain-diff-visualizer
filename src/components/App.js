81220df1fe1e34cd2c5bfd467db0eb10b785b56f
Stefan Hartmann <stefanhartmann@gmail.com>
2018-03-17 19:34:28 -0400

    successfully return & render a basic diff

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../stylesheets/Style.css';
+import Differ from './Differ.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  };
+  printDiff() {
+    var str = Differ();
+    return(
+      <div>
+        <h1>{ str }</h1>
+     </div>
+    )
+  }
   render() {
     return (
       <div className="App">
+        { this.printDiff() }
       </div>
     );
   }
